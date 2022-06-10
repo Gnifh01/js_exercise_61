bc4b3b7541b3a5f5984267e21babdb75e0871cd4
gabriele_simone <gabriele.simone1@studenti.unisalento.it>
2022-06-10 17:04:45 +0200

    first commit

---
+++ exercise-5.js
@@ -0,0 +1,69 @@
+const persons = [
+  {
+    id: 1,
+    firstName: "Mario",
+    lastName: "Rossi",
+    age: 25,
+  },
+  {
+    id: 2,
+    firstName: "Maria",
+    lastName: "Verdi",
+    age: 32,
+  },
+  {
+    id: 3,
+    firstName: "Giovanni",
+    lastName: "Rossi",
+    age: 35,
+  },
+];
+
+const jobs = [
+  {
+    id: 1,
+    jobTitle: "CEO",
+  },
+  {
+    id: 2,
+    jobTitle: "Project Manager",
+  },
+  {
+    id: 3,
+    jobTitle: "Developer",
+  },
+];
+
+// core here
+function fetchPersonById(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const person = persons.find((item) => item.id === id);
+      if (person) {
+        resolve(person);
+      }
+      reject(new Error(`id ${id} non esiste`));
+    }, 1000);
+  });
+}
+
+function fetchJobById(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const job = jobs.find((item) => item.id === id);
+      if (job) {
+        resolve(job);
+      }
+      reject(new Error(`id ${id} non esiste`));
+    }, 500);
+  });
+}
+
+async function allPromise() {
+  const id = await fetchPersonById(1);
+  console.log(id);
+  const job = await fetchJobById(1);
+  console.log(job);
+}
+
+allPromise();
